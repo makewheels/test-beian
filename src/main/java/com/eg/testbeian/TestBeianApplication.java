7836775a8365db406c1cd761b7eb0013831bc35d
bq <email>
2021-05-19 10:03:22 +0800

    https://gitee.com/ycsnail/beian

---
+++ src/main/java/com/eg/testbeian/TestBeianApplication.java
@@ -0,0 +1,13 @@
+package com.eg.testbeian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBeianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBeianApplication.class, args);
+    }
+
+}
