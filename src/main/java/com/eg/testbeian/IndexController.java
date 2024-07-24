56798b2b40a1441e919239dda741ee13bbfe6e14
miuser <miuser@email.com>
2024-07-24 10:18:13 +0800

    安康养生

--- src/main/java/com/eg/testbeian/IndexController.java
+++ src/main/java/com/eg/testbeian/IndexController.java
@@ -11,16 +11,10 @@ public class IndexController {
     @RequestMapping("/")
     public String index(HttpServletRequest request) {
         String serverName = request.getServerName();
-        if (serverName.endsWith("itube.work")) {
-            return "itube.work";
-        } else if (serverName.endsWith("shiftcheck.work")) {
-            return "shiftcheck.work";
+        if (serverName.endsWith("oneclick.video")) {
+            return "oneclick.video";
         } else if (serverName.endsWith("a4.fit")) {
             return "a4.fit";
-        } else if (serverName.endsWith("pushcenter.cc")) {
-            return "pushcenter.cc";
-        } else if (serverName.endsWith("oneclick.video")) {
-            return "oneclick.video";
         } else if (serverName.endsWith("gpt6.plus")) {
             return "gpt6.plus";
         }
